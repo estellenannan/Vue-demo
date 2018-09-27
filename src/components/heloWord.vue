9d6a9ceefdd5ff08d14489f148465be04135cb5a
TangYaNan <18839673302@163.com>
2018-09-27 23:02:32 +0800

    init

---
+++ src/components/heloWord.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  <h2 class="title">{{title}}</h2>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data () {
+     return {
+       title: 'hello Vue'
+     }
+    }
+  }
+</script>
+
+<style>
+.title {
+  color: lightcoral;
+}
+  body {
+    background: gray;
+  }
+</style>
